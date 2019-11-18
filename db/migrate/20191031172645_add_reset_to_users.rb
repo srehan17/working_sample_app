e9cfb8ebe9e7ec68546d8eeb80c2bdcf2f6a0d42
Sarah <sarahahmad17@gmail.com>
2019-11-17 22:11:08 -0500

    Retaining Temp

--- db/migrate/20191031172645_add_reset_to_users.rb
+++ db/migrate/20191031172645_add_reset_to_users.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class AddResetToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :reset_digest, :string
-    add_column :users, :reset_sent_at, :datetime
   end
 end
